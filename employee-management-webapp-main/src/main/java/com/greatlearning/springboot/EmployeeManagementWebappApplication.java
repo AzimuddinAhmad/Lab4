488d6d3b2cacf3193fa2c50ae776e64dc4ac179e
AzimuddinAhmad <105585567+AzimuddinAhmad@users.noreply.github.com>
2022-10-24 10:56:37 +0530

    Lab4
    I am submitting my lab4 solution for 50 marks. It is already verified by GL staf.
    Thanks.

---
+++ employee-management-webapp-main/src/main/java/com/greatlearning/springboot/EmployeeManagementWebappApplication.java
@@ -0,0 +1,15 @@
+//	Lab 4 for 50 marks
+//	http://localhost:8080/login
+package com.greatlearning.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementWebappApplication.class, args);
+	}
+
+}
